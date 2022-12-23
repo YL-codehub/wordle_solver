c3db3bda45a077aa54f2e955d7b1365125e277f3
Yoann <yl844@cam.ac.uk>
2022-12-23 11:30:55 +0000

    test CI

--- test_game.py
+++ test_game.py
@@ -2,7 +2,7 @@ import game as g
 wordle = g.Wordle()
 wordle.word = 'voter'
 inputs = ['aeros','camel','tight','comic','forte','magic']
-outputs = ['-yyy-','---g-','y----','-g---','-gyyy','----g']
+outputs = ['-yyy-','---g-','y----','-g---','-gyyy','-----']
 
 for i,el in enumerate(inputs):
     if wordle.evaluate_guess(el)!=outputs[i]:
